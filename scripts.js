a8436da75cc2faf744e2a9c7ad33a5dbd594acd7
Software Engineer 27 <Software.Engineer27@bafokeng.local>
2024-05-13 15:03:30 +0200

    Function to generate book

---
+++ scripts.js
@@ -0,0 +1,20 @@
+import { books, authors, genres, BOOKS_PER_PAGE } from "./data.js";
+
+let page = 1;
+let matches = books;
+
+function createBookElement(book) {
+ const { author, id, image, title } = book;
+ const element = document.createElement("button");
+ element.classList = "preview";
+ element.setAttribute("data-preview", id);
+
+ element.innerHTML = `
+        <img class="preview__image" src="${image}" />
+        <div class="preview__info">
+            <h3 class="preview__title">${title}</h3>
+            <div class="preview__author">${authors[author]}</div>
+        </div>
+    `;
+ return element;
+}
